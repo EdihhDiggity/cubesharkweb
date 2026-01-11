e5ff4d9da84b1613a3ac5d6cd18ca1f473a637f0
EdihhDiggity <diggityedihh@gmail.com>
2026-01-11 18:17:51 +0800

    Added database

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
+import os
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-import os
-from werkzeug.security import  generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "dev-only-key")
+
+# DATABASE_URL will be provided by Render for Postgres
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///User.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    _id = db.Column("id", db.Integer, primary_key = True)
-    username = db.Column(db.String(80), unique = True)
-    password = db.Column(db.String(80))
+    _id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(200))  # longer for hashed passwords
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-
-@app.route("/register", methods = ['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        
+
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             flash('Username already exists. Choose a different username.')
             return redirect(url_for('register'))
         else:
             new_user = User(
-                username = username,
-                password = generate_password_hash(password)
+                username=username,
+                password=generate_password_hash(password)
             )
             db.session.add(new_user)
             db.session.commit()
@@ -43,19 +43,16 @@ def register():
 
     return render_template("register.html")
 
-
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-# comment
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    app.debug = True
     with app.app_context():
         db.create_all()
-    app.run()
+    # Render sets PORT automatically
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
